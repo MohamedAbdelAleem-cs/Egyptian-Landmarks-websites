203807ba087c1fac870ab588decf83767f87092c
MohamedAbdelAleem-cs <MohamedAbdelAleem2001@gmail.com>
2021-07-25 13:30:57 +0200

    first commit

---
+++ admin.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".menu_bar").on('click', function () {
+        $('.nav').toggleClass('show');
+        $('.nav ul').toggleClass('show');
+        
+    });
+
+
+
+});
+
+function aa() {
+
+    document.getElementById(931321).href= "home.html";
+     
+    
+
+}
+
+
